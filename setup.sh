2e5eb7fd75c4bea8ea5b8bf1de2cd6412bf70f59
Vasyl Malanka <vsl.malanka@gmail.com>
2020-01-13 18:05:45 +0100

    Add sudo to directory ownership change commands

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ docker-compose down -v
 
 rm -rf www
 mkdir www
-chmod g+s www
-chown $(id -u):33 www
+sudo chmod g+s www
+sudo chown $(id -u):33 www
 
 docker-compose build --build-arg UID=$(id -u) php-fpm
 docker-compose build redis
